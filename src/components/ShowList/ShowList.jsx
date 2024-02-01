ad6483b4d613ed42cb6185c52f5f9e3155387ba2
Tejas Mane <tsmane8787@gmail.com>
2024-02-01 12:17:54 +0530

    UI update

--- src/components/ShowList/ShowList.jsx
+++ src/components/ShowList/ShowList.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { FaStar } from 'react-icons/fa';
+import { RiMovieFill } from "react-icons/ri";
 import './showlist.css';
 
 const ShowList = () => {
@@ -21,29 +22,33 @@ const ShowList = () => {
   const demoImageUrl = 'https://placehold.co/210x295?text=Image+Not+Available'; 
 
   return (
-    <div className="show-container">
+    <div className="homeapp">
+        <h1>QuadB <RiMovieFill className='adicons' size={18} color='red'/> </h1>
+        <h3>Discover, Rate, and Explore the World of Cinema with Us!</h3>
+        <div className="show-container">
        
-      {shows.map(({ show }) => (
-        <div key={show.id} className="show-card">
-          {show.image ? (
-            <img src={show.image.medium} alt={show.name} />
-          ) : (
-            <img src={demoImageUrl} alt="Demo" />
-          )}
-          <div className="show-details">
-            <h2>{show.name}</h2>
-            <p>
-              Rating: <FaStar size={15} color="yellow" /> {show.rating.average}
-            </p>
-            <p>Genres: {show.genres.join(', ')}</p>
-            <p>Language: {show.language}</p>
-            {/* <p>{show.summary}</p> */}
-            <Link to={`/show/${show.id}`}>
-              <button className="btn">Details</button>
-            </Link>
-          </div>
-        </div>
-      ))}
+       {shows.map(({ show }) => (
+         <div key={show.id} className="show-card">
+           {show.image ? (
+             <img src={show.image.medium} alt={show.name} />
+           ) : (
+             <img src={demoImageUrl} alt="Demo" />
+           )}
+           <div className="show-details">
+             <h2>{show.name}</h2>
+             <p>
+               Rating: <FaStar size={15} color="yellow" /> {show.rating.average}
+             </p>
+             <p>Genres: {show.genres.join(', ')}</p>
+             <p>Language: {show.language}</p>
+             {/* <p>{show.summary}</p> */}
+             <Link to={`/show/${show.id}`}>
+               <button className="btn">Details</button>
+             </Link>
+           </div>
+         </div>
+       ))}
+     </div>
     </div>
   );
 };
